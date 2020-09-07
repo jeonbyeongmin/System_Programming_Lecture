6f9637763abd1f2e3731a5bb656065c977af9e79
jeonbyeongmin <qudals7613@gmail.com>
2020-09-08 00:11:42 +0900

    test

---
+++ practice/HelloC/HelloC/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloC
+//
+//  Created by 전병민 on 2020/09/07.
+//  Copyright © 2020 전병민. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
